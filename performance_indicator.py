9ecbc41eee1491844ae82d9575cfd74a4c267c46
“MC-Kangan” <kc719@ic.ac.uk>
2024-03-17 17:34:45 +0000

    File backup

--- performance_indicator.py
+++ performance_indicator.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 
     
-
+# https://www.codearmo.com/blog/sharpe-sortino-and-calmar-ratios-python
+# https://blog.quantinsti.com/sharpe-ratio-applications-algorithmic-trading/
 def compute_sharpe_ratio(excess_return: np.ndarray) -> float:
     """
     Compute the Sharpe Ratio for a given series of excess returns.
@@ -14,7 +15,7 @@ def compute_sharpe_ratio(excess_return: np.ndarray) -> float:
     float: The Sharpe Ratio, calculated as the mean of the excess returns divided by their standard deviation.
     """
     # Calculating the Sharpe Ratio
-    sr = np.mean(excess_return) / np.std(excess_return)
+    sr = np.mean(excess_return) / np.std(excess_return) * np.sqrt(len(excess_return))
     return sr
 
 def compute_rolling_sharpe_ratio(excess_return: np.ndarray, window: int) -> np.ndarray:
@@ -36,7 +37,7 @@ def compute_rolling_sharpe_ratio(excess_return: np.ndarray, window: int) -> np.n
     rolling_std = temp_df['excess_return'].rolling(window = window).std().to_numpy()
 
     # Calculating the rolling Sharpe Ratio
-    rolling_sr = rolling_mean / rolling_std
+    rolling_sr = rolling_mean / rolling_std * np.sqrt(window)
     return rolling_sr
 
 def compute_sortino_ratio(excess_return: np.ndarray) -> float:
@@ -56,48 +57,129 @@ def compute_sortino_ratio(excess_return: np.ndarray) -> float:
     downside_std = np.std(negative_excess_returns)
 
     # Calculating the Sortino Ratio
-    sortino_ratio = np.mean(excess_return) / downside_std
+    sortino_ratio = np.mean(excess_return) / downside_std * np.sqrt(len(excess_return))
     return sortino_ratio
 
-def compute_drawdown(portfolio_value: np.ndarray, max_drawdown: bool = True) -> np.ndarray:
+def compute_drawdown(excess_return: np.ndarray, max_drawdown: bool = True) -> np.ndarray:
     """
     Compute drawdown or maximum drawdown of a portfolio.
 
     Parameters:
-    portfolio_value (np.ndarray): Array of portfolio values over time.
+    excess_return (np.ndarray): Array of portfolio values over time.
     max_drawdown (bool): If True, returns the maximum drawdown, otherwise returns the drawdown series.
 
     Returns:
     np.ndarray: Either the maximum drawdown value or an array of drawdown values over time.
     """
-    # Calculating the running maximum of the portfolio value
-    running_max = np.maximum.accumulate(portfolio_value)
+    
+    if not isinstance(excess_return, np.ndarray):
+        excess_return = np.array(excess_return)
+    
+    drawdown = np.zeros(len(excess_return))
+    peak = excess_return[0]
 
-    # Calculating the drawdown
-    drawdown = (running_max - portfolio_value) / running_max
+    for i in range(1, len(excess_return)):
+        if excess_return[i-1] == 0:
+            continue
+        peak = max(peak, excess_return[i])
+        if peak == 0:
+            peak = np.finfo(float).eps # Smallest positive float number
+        
+        drawdown[i] = (excess_return[i] - peak) / peak
 
-    # Returning either the maximum drawdown or the drawdown series
-    if not max_drawdown:
-        return drawdown
+    if max_drawdown:
+        return np.min(drawdown)
     else:
-        return np.max(drawdown)
+        return drawdown
+    
+def compute_drawdown_pnl(pnl_series: np.ndarray) -> np.ndarray:
+    """
+    Calculate the drawdown of a PnL series.
 
-def compute_calmar_ratio(excess_return: np.ndarray, portfolio_value: np.ndarray) -> float:
+    Parameters:
+    pnl_series (np.ndarray): An array of cumulative profits and losses.
+
+    Returns:
+    np.ndarray: An array of drawdown values.
+    """
+    # Calculate the cumulative maximum of the PnL series up to each point
+    running_max = np.maximum.accumulate(pnl_series)
+    # Calculate drawdown
+    drawdown = np.log(running_max - pnl_series)
+
+    return drawdown    
+
+def compute_max_drawdown(
+        daily_excess_returns : np.ndarray, 
+        cummulative_returns : np.ndarray 
+        ) -> float:
+    """
+    Calculates the maximum drawdown of a strategy as a percentage
+
+    Arguments:
+    ----------
+        daily_excess_returns    : {np.ndarray}
+                                    > The daily excess returns of a strategy
+                                    (already discounted by the risk-free rate)
+        cummulative_returns     : {np.ndarray}
+                                    > The cummulative returns of a strategy
+
+    Returns:
+    ----------
+        max_drawdown            : {float}
+                                    > The maximum drawdown of a strategy
+                                    (already discounted by the risk-free rate)
+    """
+    # Compute the percentage returns
+    pect_returns = daily_excess_returns / cummulative_returns
+    
+    # Compute the cumulative returns and the running maximum
+    cum_returns = np.cumprod(1 + pect_returns)
+    running_max = np.maximum.accumulate(cum_returns)
+    
+    # Compute the drawdowns and find the maximum drawdown
+    drawdowns = (cum_returns / running_max) - 1
+    max_drawdown = np.min(drawdowns)
+
+    return max_drawdown
+
+
+
+def compute_calmar_ratio(excess_return: np.ndarray) -> float:
     """
     Compute the Calmar Ratio for a given series of excess returns and portfolio values.
 
     Parameters:
     excess_return (np.ndarray): Array of excess returns for a portfolio.
-    portfolio_value (np.ndarray): Array of portfolio values over time.
+    excess_return (np.ndarray): Array of portfolio values over time.
 
     Returns:
     float: The Calmar Ratio, calculated as the standard deviation of the excess returns divided by the maximum drawdown.
     """
     # Calculating the standard deviation of the excess returns
-    CR = np.mean(excess_return)
-
+    
+    mean_return = np.mean(excess_return)
     # Calculating the Calmar Ratio
-    return CR / compute_drawdown(portfolio_value, max_drawdown=True)
+    return mean_return / compute_drawdown(excess_return, True)
+
+
+def compute_calmar_ratio_modify(daily_pnl: np.ndarray, cum_pnl: np.ndarray) -> float:
+    """
+    Compute the Calmar Ratio for a given series of excess returns and portfolio values.
+
+    Parameters:
+    excess_return (np.ndarray): Array of excess returns for a portfolio.
+    excess_return (np.ndarray): Array of portfolio values over time.
+
+    Returns:
+    float: The Calmar Ratio, calculated as the standard deviation of the excess returns divided by the maximum drawdown.
+    """
+    # Calculating the standard deviation of the excess returns
+    
+    return_series = daily_pnl / cum_pnl
+    mean_return = np.mean(return_series) * len(return_series)
+    # Calculating the Calmar Ratio
+    return mean_return / abs(compute_max_drawdown(daily_pnl, cum_pnl))
 
      
     
--- trading_computation.py
+++ trading_computation.py
@@ -27,7 +27,9 @@ def compute_position_value(df: pd.DataFrame,
     V, Vcap = np.zeros(N), np.zeros(N)
     dV, dVcap, dVtot = np.zeros(N), np.zeros(N), np.zeros(N)
 
-    V[0] = initial_capital
+    # 95% of money is used to trade and 5% of money is left in Vcap for emergency
+    V[0] = initial_capital * 0.95
+    Vcap[0] = initial_capital * 0.05
     Vtot = V + Vcap
 
     # theta: dollar value of ETF held at time t (How much money are you invested into the ETF)
@@ -36,7 +38,7 @@ def compute_position_value(df: pd.DataFrame,
     for t, signal in enumerate(signal):
         if t == 0:
             dVcap[t] = Vcap[t] * daily_EFFR[t]
-            Vtot[t] = Vcap[t] + dVcap[t]
+            Vtot[t] = Vcap[t] + dVcap[t] + V[t]
             continue
         
         if signal > 0:
@@ -58,18 +60,6 @@ def compute_position_value(df: pd.DataFrame,
 
         dV[t] = daily_excess_return[t] * theta[t]
         V[t] = V[t-1] + dV[t]
-
-        M[t] = np.abs(theta[t])/leverage # Total margin used
-
-        dVcap[t] = (Vtot[t-1] - M[t]) * daily_EFFR[t] 
-        
-        # Vtot[t-1] - M[t] refers to the total amount of money not being used to invest in ETF, this value should not be less than 0
-        # if dVcap[t] < 0:
-        #     dVcap[t] = 0
-        Vcap[t] = Vcap[t-1] + dVcap[t]
-
-        dVtot[t] = dV[t] + dVcap[t]
-        Vtot[t] = Vtot[t-1] + dVtot[t]
         
         # If the position value is greater than the bound, which is +- Vtot * L
         if np.abs(theta[t]) > V[t] * leverage:
@@ -83,6 +73,22 @@ def compute_position_value(df: pd.DataFrame,
             unit[t] -= np.sign(theta[t]) * diffs / price[t]
             theta[t] -= np.sign(theta[t]) * diffs
             Vcap[t] += np.sign(theta[t]) * diffs
+            
+            # Recalculate dV and V after adjustment
+            dV[t] = daily_excess_return[t] * theta[t]
+            V[t] = V[t-1] + dV[t]
+
+        M[t] = np.abs(theta[t])/leverage # Total margin used
+
+        dVcap[t] = (Vtot[t-1] - M[t]) * daily_EFFR[t] 
+        
+        # Vtot[t-1] - M[t] refers to the total amount of money not being used to invest in ETF, this value should not be less than 0
+        # if dVcap[t] < 0:
+        #     dVcap[t] = 0
+        Vcap[t] = Vcap[t-1] + dVcap[t]
+        dVtot[t] = dV[t] + dVcap[t]
+        Vtot[t] = Vtot[t-1] + dVtot[t]
+        
         
     return {'Vtot': Vtot,'Vcap': Vcap, 'V': V,
             'dVtot': dVtot,'dVcap': dVcap, 'dV': dV,
@@ -125,5 +131,79 @@ def calculate_PnL(Vtot: np.ndarray):
     return np.diff(Vtot)
     
 
+# Reference version (NOT USED)
+def evaluate_strategy(df, signals, init_capital=200000, max_leverage=10, control_leverage=False, hold=False):
+    trade_days = len(df)
 
-    
+    prices = df['Close'].to_numpy()
+    effrs = df['Daily EFFR'].to_numpy()
+    excess_returns = df['Excess return'].to_numpy()
+
+    units = np.zeros(trade_days)
+    theta = np.zeros(trade_days)
+    margins = np.zeros(trade_days)
+
+    dV = np.zeros(trade_days)
+    dV_cap = np.zeros(trade_days)
+    dV_total = np.zeros(trade_days)
+
+    V = np.zeros(trade_days)
+    V[0] = init_capital
+    V_cap = np.zeros(trade_days)
+    V_total = V + V_cap
+
+    for t, signal in enumerate(signals):
+        # First day, no trade
+        if t == 0:
+            V_cap[t] = V_total[t] - margins[t]
+            dV_cap[t] = V_cap[t] * effrs[t]
+            dV_total[t] = dV_cap[t] + dV[t]
+            continue
+
+        V_total[t] = V_total[t-1] + dV_total[t-1]
+        V[t] = V[t-1] + dV[t-1]
+
+        if signal == 1:
+            leverage = max_leverage
+        else:
+            leverage = max_leverage / 2 if control_leverage else max_leverage
+
+        if signal == 0 and hold:
+            units[t] = units[t-1]
+            theta[t] = units[t] * prices[t]
+        else:
+            theta[t] = signal * V[t] * leverage
+            units[t] = theta[t] / prices[t]
+
+        # Adjust the theta according to the trade value before calculating the pnl
+        # If theta > V * leverage, adjust the units held, cut the excess value, save it to capital,
+        # If theta < -V * leverage, adjust the units held, cut the excess value from capital.
+        # If not enough money in capital, out of trading.
+        if np.abs(theta[t]) > V[t] * leverage:
+            diffs = np.abs(theta[t]) - V[t] * leverage
+            units[t] -= np.sign(theta[t]) * diffs / prices[t]
+            theta[t] -= np.sign(theta[t]) * diffs
+            V_cap[t] += np.sign(theta[t]) * diffs
+
+        # Calculate the margin used and get the final capital value
+        margins[t] = np.abs(theta[t]) / leverage
+        V_cap[t] += V_total[t] - margins[t]
+
+        # Calculate the daily pnl
+        dV[t] = theta[t] * excess_returns[t]
+        dV_cap[t] = V_cap[t] * effrs[t]
+        dV_total[t] = dV[t] + dV_cap[t]
+
+    wrapped_data = {
+        'units_held': units,
+        'margins': margins,
+        'trade_pnl': dV,
+        'cap_pnl': dV_cap,
+        'total_pnl': dV_total,
+        'theta': theta,
+        'trade_vals': V,
+        'cap_vals': V_cap,
+        'total_vals': V_total
+    }
+
+    return wrapped_data
